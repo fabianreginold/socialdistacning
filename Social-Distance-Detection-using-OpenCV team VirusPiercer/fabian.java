775d604497600a99e490b3079fb419c276656be5
fabianreginold <fabianreginold@gmail.com>
2020-05-30 16:16:19 +0530

    Add files via upload

---
+++ Social-Distance-Detection-using-OpenCV team VirusPiercer/fabian.java
@@ -0,0 +1,15 @@
+class fabian
+
+{
+    void display()
+    {
+        System.out.println("hello first one");
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("hello");
+        fabian obj=new fabian();
+        obj.secondtrial();
+    }
+}
